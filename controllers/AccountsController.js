2f1833fa94ce1a0d4ea55561122eb75d62ab920c
Nicolas Chourot <45172002+Nicolas-Chourot@users.noreply.github.com>
2023-12-04 08:48:30 -0500

    Update AccountsController.js

--- controllers/AccountsController.js
+++ controllers/AccountsController.js
@@ -55,7 +55,6 @@ export default class AccountsController extends Controller {
     }
     sendVerificationEmail(user) {
         // bypass model bindeExtraData wich hide the user verifyCode
-        // user = this.repository.findByField("Id", user.Id);
         let html = `
                 Bonjour ${user.Name}, <br /> <br />
                 Voici votre code pour confirmer votre adresse de courriel
@@ -115,11 +114,13 @@ export default class AccountsController extends Controller {
     register(user) {
         if (this.repository != null) {
             user.Created = utilities.nowInSeconds();
-            user.VerifyCode = utilities.makeVerifyCode(6);
+            let verifyCode = utilities.makeVerifyCode(6);
+            user.VerifyCode = verifyCode;
             user.Authorizations = Authorizations.user();
             let newUser = this.repository.add(user);
             if (this.repository.model.state.isValid) {
                 this.HttpContext.response.created(newUser);
+                newUser.Verifycode = verifyCode;
                 this.sendVerificationEmail(newUser);
             } else {
                 if (this.repository.model.state.inConflict)
